44c82b12cfd7b4a83bbdcadc80d6b96c27f1081f
johandi <limjohandi@gmail.com>
2022-05-14 15:46:00 +0700

    postgresql tested

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const Pool = require("pg").Pool;
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    pool.query(`select * from users`)
+    .then((data)=>{
+        res.json(data.rows)
+    })
+    .catch((err)=>{
+        res.send(`Failed, ${err}`)
+    })
   })
   
 app.listen(PORT, ()=>{
